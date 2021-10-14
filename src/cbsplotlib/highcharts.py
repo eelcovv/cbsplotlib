2b8c6e241791079630731333f9578629fc2723b3
EVLT <>
2021-10-14 10:37:56 +0200

    modificatie template niet meer mogelijk

--- src/cbsplotlib/highcharts.py
+++ src/cbsplotlib/highcharts.py
@@ -1,3 +1,4 @@
+import sys
 import codecs
 import logging
 import json
@@ -25,13 +26,14 @@ class CBSHighChart:
                  output_file_name: str = None,
                  output_directory: str = None,
                  defaults_directory: str = None,
-                 defaults_filename: str = None,
-                 modifications_filename: str = None,
+                 defaults_file_name: str = None,
+                 defaults_out_file: str = None,
                  chart_type: str = None,
                  csv_separator: str = ";",
                  decimal: str = ",",
                  index_col: int = 0,
                  y_format: str = None,
+                 start: bool = False,
                  chart_title: str = None,
                  chart_subtitle: str = None,
                  chart_title_font_family="\"Soho W01 Medium\", \"Cambria\", sans-serif",
@@ -57,35 +59,62 @@ class CBSHighChart:
         else:
             self.chart_type = chart_type
 
-        self.output_filename = output_file_name
+        if output_directory is None:
+            self.output_directory = Path(".")
+        else:
+            self.output_directory = Path(output_directory)
+
+        self.output_file_name = output_file_name
+
+        self.modification_file_name = defaults_out_file
+
+        # initieer de output dictionary
         self.output = {
             "template": {},
             "options": {},
             "selectedTemplate": {}
         }
 
-        if output_directory is None:
-            self.output_directory = Path(".")
-        else:
-            self.output_directory = Path(output_directory)
-
         # hier worden alle defaults in de self.defaults attribute geladen
         self.defaults = self.read_the_defaults(chart_type=chart_type,
                                                defaults_directory=defaults_directory,
-                                               defaults_filename=defaults_filename,
-                                               modifications_filename=modifications_filename)
+                                               defaults_file_name=defaults_file_name)
+
+        if defaults_out_file is not None:
+            # als we een default output file geven dan schrijven we alleen de huidige template naar deze file
+            self.write_to_file(output=self.defaults, output_file_name=defaults_out_file)
+            _logger.info(
+                """
+                We hebben de defaults template geschreven om het *default_out_file* als argument gegeven
+                was. Als je het plaatje met deze template wil maken, geef dan *defaults_out_file* mee met het 
+                *defaults_file_name* argument. Script stop nu hier.
+                """
+            )
+            return
 
         # get the data here, if take from the argument
         if data is None:
-            _logger.debug(f"Reading dataframe from {input_file_name}")
+            _logger.info(f"Reading data from {input_file_name}")
             self.data_df = self.get_data(input_file_name, index_col=index_col, csv_separator=csv_separator,
-                                    decimal=decimal)
+                                         decimal=decimal)
         else:
             _logger.debug(f"Using dataframe")
             self.data_df = data
 
         # get the categories from the data
-        categories = self.get_categories()
+        self.categories = self.get_categories()
+
+        _logger.debug(f"categories: {self.categories}")
+
+        if start:
+            # als de start optie meegeven is dan gaan we de highcharts file bouwen
+            self.make_highcharts()
+            _logger.info("Done with making highcharts")
+        else:
+            _logger.info("The data was successfully. To create the highcharts, call the *make_highcharts()* method"
+                         "or pass the start=True argument")
+
+    def make_highcharts(self):
 
         # now add all the items of the highcarts
         self.add_chart()
@@ -94,7 +123,7 @@ class CBSHighChart:
         self.add_title(text_key="title")
         self.add_title(text_key="subtitle")
 
-        self.add_axis(axis_key="xAxis", categories=categories)
+        self.add_axis(axis_key="xAxis", categories=self.categories)
         self.add_axis(axis_key="yAxis")
         self.add_legend()
         self.add_tooltip()
@@ -103,13 +132,17 @@ class CBSHighChart:
         self.add_options()
         self.add_csv_data()
         self.add_series()
-        self.add_axis(key="options", axis_key="xAxis", categories=categories)
+        self.add_axis(key="options", axis_key="xAxis", categories=self.categories)
         self.add_axis(key="options", axis_key="yAxis")
 
         self.add_selected_templated()
 
         # finally write the result to file
-        self.write_to_file()
+        self.write_to_file(output=self.output,
+                           output_directory=self.output_directory,
+                           output_file_name=self.output_file_name,
+                           input_file_name=self.input_file_name,
+                           chart_type=self.chart_type)
 
     @staticmethod
     def get_data(input_file_name, index_col=0, csv_separator=";", decimal=","):
@@ -117,7 +150,7 @@ class CBSHighChart:
             raise TypeError("Both input data argument *data_df* and input filename "
                             "*input_file_name* are None. Please provide at least one.")
         else:
-            _logger.info(f"Reading {input_file_name}")
+            _logger.debug(f"Reading {input_file_name}")
             data_df = pd.read_csv(input_file_name,
                                   sep=csv_separator,
                                   index_col=index_col,
@@ -139,48 +172,47 @@ class CBSHighChart:
         return categories
 
     @staticmethod
-    def read_the_defaults(chart_type=None,
-                          defaults_directory=None,
-                          defaults_filename=None,
-                          modifications_filename=None):
+    def read_the_defaults(chart_type: str = None,
+                          defaults_directory: str = None,
+                          defaults_file_name: str = None,
+                          cbs_hc_defaults: str = "cbs_hc_defaults"):
+        """
+        Lees de settings uit een default template json en overschrijf met de instellingen uit een modification json
 
-        def modify_the_defaults(mod_defaults, mods):
-
-            for chapter_key, chapter_prop in mods:
-                for section_key, section_prop in chapter_prop:
-                    if isinstance(section_prop, dict):
-                        for subsection_key, subsection_prop in section_prop:
-                            mod_defaults[chapter_key][section_key][subsection_key] = subsection_prop
-                    else:
-                        mod_defaults[chapter_key][section_key] = section_prop
-            return mod_defaults
+        Parameters
+        ----------
+        chart_type: str of None
+            Type plot die we maken (bar, column, line). De default template wordt gebaseerd hierop gemaakt
+        defaults_directory: str of None
+            Locatie waar de default template staat. Als niet gegeven en default file naam is niet mee gegeven, dan
+            is dit <PACKAGELOCATIE>/Path(cbs_hc_defaults)
+        defaults_file_name: str of None
+            Naam van de defaults input file. Als niet gegeven wordt deze gebaseerd op *chart_type*
+        cbs_hc_defaults: str
+            Naam van de default directory in de package src.
+        """
 
         if defaults_directory is None:
-            defaults_directory = Path(__file__).parent / Path("cbs_hc_defaults")
+            if defaults_file_name is None:
+                # er is geen default filenaam meegegeven. Ga ervan uit dat we de default uit de package folder halen
+                defaults_directory = Path(__file__).parent / Path(cbs_hc_defaults)
         else:
             defaults_directory = Path(defaults_directory)
 
-        if defaults_filename is None:
-            defaults_filename = defaults_directory / Path(chart_type + ".json")
+        if defaults_file_name is None:
+            # alde default filename None is dan is default directory sowieso gezet
+            defaults_file_name = defaults_directory / Path(chart_type + ".json")
         else:
-            defaults_filename = defaults_directory / Path(defaults_filename)
-
-        if modifications_filename is not None:
-            modifications_filename = Path(modifications_filename)
-
-        with open(defaults_filename, "r") as stream:
-            defaults = json.load(stream)
-
-        if modifications_filename is not None:
-            if modifications_filename.exists():
-                _logger.info(f"Reading modification settings from {modifications_filename} ")
-                with open(modifications_filename, "r") as stream:
-                    modifications = json.load(stream)
-                defaults = modify_the_defaults(defaults, modifications)
+            # default file naam was door de gebruiker meegegeven. Als ook de directory meegegeven was dan combineren
+            # we de naam met de directory, anders nemen we direct de filenaam
+            if defaults_directory is None:
+                defaults_file_name = Path(defaults_file_name)
             else:
-                _logger.info(f"Creating new modification settings file {modifications_filename}")
-                with codecs.open(modifications_filename.as_posix(), "w", encoding='utf-8') as fp:
-                    fp.write(json.dumps(defaults, indent=2, ensure_ascii=False))
+                defaults_file_name = defaults_directory / Path(defaults_file_name)
+
+        _logger.info(f"Reading template {defaults_file_name}")
+        with open(defaults_file_name, "r") as stream:
+            defaults = json.load(stream)
 
         return defaults
 
@@ -262,18 +294,50 @@ class CBSHighChart:
     def add_selected_templated(self, key="selectedTemplate"):
         self.output[key] = self.defaults[key]
 
-    def write_to_file(self, json_indent=2):
-        self.output_directory.mkdir(exist_ok=True)
-        if self.output_filename is None:
-            if self.input_file_name is None:
-                default_file_name = Path("_".join(["highchart", self.chart_type, "plot"]) + ".json")
+    @staticmethod
+    def write_to_file(output,
+                      json_indent=2,
+                      output_directory: Path = None,
+                      output_file_name: str = None,
+                      input_file_name: str = None,
+                      chart_type=None):
+        """
+        Write the dictionary to an json output file
+        Parameters
+        ----------
+        output: dict
+            The settings dictionary to write
+        json_indent: int
+            Number of indenting spaces. If None is give, a compact json is written
+        output_directory: Path of None
+            Output directory
+        output_file_name: str of None
+            Naam van de output file
+        input_file_name: str of None
+            Naam van de input data file. Als we geen output filenaam geven zal de output file hierop gebaseerd worden.
+        chart_type: str
+            Type plot (bar, column, line). Als gegeven wordt dit in de filenaam verwerkt.
+
+        """
+        if output_directory is not None:
+            output_directory.mkdir(exist_ok=True)
+        if output_file_name is None:
+            if input_file_name is None:
+                if chart_type is None:
+                    chart_label = ""
+                else:
+                    chart_label = chart_type
+                default_file_name = Path("_".join(["highchart", chart_label, "plot"]) + ".json")
             else:
-                file_stem = Path(self.input_file_name).stem
+                file_stem = Path(input_file_name).stem
                 default_file_name = Path(file_stem).with_suffix(".json")
 
-            outfile = self.output_directory / default_file_name
+            outfile = output_directory / default_file_name
         else:
-            outfile = self.output_directory / Path(self.output_filename).with_suffix(".json")
+            if output_directory is None:
+                outfile = Path(output_file_name).with_suffix(".json")
+            else:
+                outfile = output_directory / Path(output_file_name).with_suffix(".json")
         _logger.info(f"Writing to {outfile}")
         with codecs.open(outfile.as_posix(), "w", encoding='utf-8') as stream:
-            stream.write(json.dumps(self.output, indent=json_indent, ensure_ascii=False))
+            stream.write(json.dumps(output, indent=json_indent, ensure_ascii=False))
