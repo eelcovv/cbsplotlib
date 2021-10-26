72d280ab30a03187888752664717bcb37f6b316e
Eelco van Vliet <eelcovv@gmail.com>
2021-10-26 14:30:18 +0200

    impose gridline color

--- src/cbsplotlib/settings.py
+++ src/cbsplotlib/settings.py
@@ -111,6 +111,7 @@ class CBSPlotSettings(object):
                       'xtick.labelsize': font_size,
                       'ytick.labelsize': font_size,
                       'figure.figsize': self.fig_size,
+                      'grid.color': 'cbs:highchartslichtgrijs',
                       'hatch.color': 'cbs:highchartslichtgrijs',
                       'axes.prop_cycle': get_color_palette(color_palette),
                       'axes.edgecolor': "cbs:grijs",
