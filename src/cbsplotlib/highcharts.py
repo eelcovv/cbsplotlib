14d3cf4a075be7d46fca457247c07db80973ac2a
Eelco van Vliet <eelcovv@gmail.com>
2021-10-26 06:30:25 +0200

    has grouped categories vlag gezet op basis van index level

--- src/cbsplotlib/highcharts.py
+++ src/cbsplotlib/highcharts.py
@@ -384,8 +384,12 @@ class CBSHighChart:
     def get_categories(self):
         if self.data_df.index.nlevels == 1:
             categories = [str(_) for _ in self.data_df.index.to_list()]
+            if self.has_grouped_categories is None:
+                self.has_grouped_categories = False
         elif self.data_df.index.nlevels == 2:
             categories = list()
+            if self.has_grouped_categories is None:
+                self.has_grouped_categories = True
 
             for first_level_key, df in self.data_df.groupby(level=0, sort=False):
                 categories_strings = [str(_) for _ in df.index.get_level_values(1).to_list()]
