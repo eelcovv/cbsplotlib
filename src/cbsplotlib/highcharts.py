2b090b36e26773fed5881639fe3b19af00362ef5
Eelco van Vliet <eelcovv@gmail.com>
2022-06-13 20:03:57 +0200

    global warming voorbeeld toegevoegd voor lijnplot en categories nu altijd als string

--- src/cbsplotlib/highcharts.py
+++ src/cbsplotlib/highcharts.py
@@ -561,7 +561,7 @@ class CBSHighChart:
 
         if categories is not None:
             for ax in self.output[key][axis_key]:
-                ax["categories"] = str(categories)
+                ax["categories"] = [str(c) for c in categories]
 
     def add_options(self):
         self.output["options"] = self.defaults["options"]
